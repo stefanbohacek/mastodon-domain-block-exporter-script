fd8a874838f38bf64679729fd74be069e8d4a216
Stefan Bohacek <stefan@stefanbohacek.com>
2024-06-26 20:12:57 -0400

    Display current status of what the script is doing.

--- script.py
+++ script.py
@@ -9,14 +9,17 @@ parser.add_argument("reject_media", help="Should media attachments be rejected? 
 parser.add_argument("reject_reports", help="Should reports from this domain be rejected? true/false", type=str)
 parser.add_argument("obfuscate", help="Should the domain name be partially censored when shown publicly? true/false", type=str)
 args = parser.parse_args()
-print(args.domain)
 
 if (args.domain):
+    print(f"downloading domain blocks from {args.domain}...")
+
     with urllib.request.urlopen(f"http://{args.domain}/api/v1/instance/domain_blocks") as url:
 
         data = json.load(url)
         domain_blocks = []
         # print(json_data)
+        print("processing...")
+
         for item in data:
             domain_blocks.append({
                 "#domain": item["domain"],
@@ -27,8 +30,13 @@ if (args.domain):
                 "#obfuscate": args.obfuscate
             })
 
-        export_file = open(f"{args.domain}-domain-blocks.csv", "w")
+
+        file_name = f"{args.domain}-domain-blocks.csv"
+        print(f"saving to {file_name}...")
+
+        export_file = open(file_name, "w")
         writer = DictWriter(export_file, domain_blocks[0].keys())
         writer.writeheader()
         writer.writerows(domain_blocks)
         export_file.close()
+        print("done!")
